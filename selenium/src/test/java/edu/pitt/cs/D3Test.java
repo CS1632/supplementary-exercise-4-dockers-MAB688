2a2f3f0784583506021483f6ce933c3781072011
Mark Bosco <mab688@pitt.edu>
2023-11-15 10:27:37 -0500

    x

--- selenium/src/test/java/edu/pitt/cs/D3Test.java
+++ selenium/src/test/java/edu/pitt/cs/D3Test.java
@@ -36,14 +36,14 @@ public class D3Test {
   JavascriptExecutor js;
   @Before
   public void setUp() {
-    driver = new FirefoxDriver();
-    js = (JavascriptExecutor) driver;
-    vars = new HashMap<String, Object>();
-    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
-    
     ChromeOptions options = new ChromeOptions();
     options.addArguments("--headless");
+    
     driver = new ChromeDriver(options);
+    js = (JavascriptExecutor) driver;
+    vars = new HashMap<>();
+    
+    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
   }
   @After
   public void tearDown() {
